ab6e5b24d4a395858af2fd763ff7d7a4af142575
carlos_reyes <carlosarp2000@gmail.com>
2021-11-08 20:51:17 -0500

    curso js tipos de datos

---
+++ app.js
@@ -0,0 +1,18 @@
+// tipos de datos
+
+var numbers = 1
+var strings = "Strings"
+var booleans = true
+var arrays = ['joe', 'ryan', 'martha']
+
+var objetos = {
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "professional": true
+}
+
+
+console.log(objetos.username)
+
+
